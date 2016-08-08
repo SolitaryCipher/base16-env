f8ea50b6cd362bbfd704cb41e31cc1f148961e33
Nick Bear <nick@tunnelbear.com>
2016-08-08 16:58:37 -0400

    edited fish script

--- base16-fish.fish
+++ base16-fish.fish
@@ -1,9 +1,9 @@
 #!/usr/bin/fish
 # script to set fish prompt colors, assuming env is set
 
-set -g fish_color_autosuggestion    $base03
+set -g fish_color_autosuggestion    brgrey
 set -g fish_color_command           magenta
-set -g fish_color_comment           $base04
+set -g fish_color_comment           brgrey
 set -g fish_color_cwd               green
 set -g fish_color_cwd_root          red
 set -g fish_color_end               magenta
@@ -16,8 +16,8 @@ set -g fish_color_operator          cyan
 set -g fish_color_param             cyan
 set -g fish_color_quote             green
 set -g fish_color_redirection       yellow
-set -g fish_color_search_match      --background=$base03
-set -g fish_color_selection         --background=purple
+set -g fish_color_search_match      --background=454545
+set -g fish_color_selection         --background=magenta
 set -g fish_color_valid_path        --underline
 #set -g fish_pager_color_completion  normal
 #set -g fish_pager_color_description '555'  'yellow'
